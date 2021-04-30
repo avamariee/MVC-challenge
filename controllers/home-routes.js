cbbf43c1c375eaa3f97192016b48219f69f4a95a
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-29 22:45:19 -0600

    rendered posts on the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,44 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'body',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // pass post object onto the homepage
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(dbPostData[0])
+
+        res.render('homepage', {posts});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path')
 const exphbs = require('express-handlebars');
 const session = require('express-session')
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
