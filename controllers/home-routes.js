21250f57035e72b21b01980854c72a499a245cf7
Ava Marie Bullock <ava.m.bullock@gmail.com>
2021-04-30 21:26:26 -0600

    successfully added comments and single-post display

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -47,5 +47,56 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// find a single post
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'body',
+            'created_at'
+            
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'There is no post with this ID!' });
+                return;
+            }
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('show-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 
 module.exports = router;
